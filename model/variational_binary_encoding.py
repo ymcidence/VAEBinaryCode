352c230ab2dd8e18497dbde207af2107c010d711
ymcidence <ym_zmxncbv@hotmail.com>
2017-03-03 20:40:12 +0000

    asdasdsad

--- model/variational_binary_encoding.py
+++ model/variational_binary_encoding.py
@@ -5,6 +5,7 @@ from util import layers
 NAME_SCOPE_VARIATION = 'VariationalNet'
 NAME_SCOPE_GENERATION = 'GenerativeNet'
 NAME_SCOPE_RECOGNITION = 'RecognitionNet'
+NAME_SCOPE_CODE_LEARNING = 'CodeLearning'
 
 
 def variational_net(input_tensor, latent_size):
@@ -35,7 +36,7 @@ def generative_net(input_tensor):
         fc_1 = layers.fc_relu_layer('Fc1', input_tensor, 1024)
         fc_2 = layers.fc_relu_layer('Fc2', fc_1, 1024)
         fc_3_mean = layers.fc_layer('Fc3Mean', fc_2, 1024)
-        fc_3_sigma = layers.fc_layer('Fc3Sigma', fc_2, 1024)
+        fc_3_sigma = layers.fc_layer('Fc3LogSigma', fc_2, 1024)
     return fc_3_mean, fc_3_sigma
 
 
@@ -65,22 +66,51 @@ def loss_kl(variational_mean, variational_log_sigma):
     return variational_loss
 
 
-def loss_px(generated_mean, generated_sigma, real_data):
+def loss_px(generated_mean, generated_log_sigma, real_data):
     """
     p(x|z)
     :param generated_mean:
-    :param generated_sigma:
+    :param generated_log_sigma:
     :param real_data:
     :return:
     """
     c = -0.5 * cmath.log(2 * cmath.pi)
-    generative_loss = tf.square(real_data - generated_mean) / (2. * tf.exp(generated_sigma)) - c + generated_sigma / 2.
+    generative_loss = tf.square(real_data - generated_mean) / (
+        2. * tf.exp(generated_log_sigma)) - c + generated_log_sigma / 2.
     return generative_loss
 
 
+def binary_code_learning_process(matrix_f, orthogonal_rotation, aux_binaries, code_length, iteration=3):
+    def loop_body(f, r, b):
+        a, _, c = tf.svd(tf.matmul(b, f, transpose_b=True))
+        r = tf.matmul(a, c)
+        b = tf.sign(tf.matmul(r, f))
+
+        with tf.variable_scope(NAME_SCOPE_CODE_LEARNING, reuse=True):
+            bins = tf.cast(tf.greater(tf.random_normal([code_length, code_length]), 0), tf.float32)
+            assign_b = tf.assign(tf.get_variable('MatrixB'), bins)
+
+    with tf.control_dependencies([assign_b]):
+
+    return 0
+
+
 class BinaryEncodingVae(object):
-    def __init__(self, code_length, sess=tf.Session()):
+    def __init__(self, code_length, sess=tf.Session(), feature_length=1024, label_num=60):
         self.sess = sess
         self.code_length = code_length
-        self.data_feature = tf.placeholder(tf.float32, [None, None])
-        self.matrix_r = tf.Variable(initial_value=tf.eye(code_length, code_length), trainable=False)
+        self.data_feature = tf.placeholder(tf.float32, [None, feature_length])
+        self.data_label = tf.placeholder(tf.float32, [None, label_num])
+        with tf.variable_scope(NAME_SCOPE_CODE_LEARNING):
+            self.matrix_r = tf.Variable(initial_value=tf.eye(code_length, code_length), name='MatrixR', trainable=False)
+            self.matrix_b = tf.Variable(initial_value=tf.random_normal(code_length, code_length), name='MatrixB',
+                                        trainable=False)
+
+    def _build_graph(self):
+        variational_mean, variational_log_sigma = variational_net(self.data_feature, self.code_length)
+        batch_size = self.data_feature.get_shape().as_list()[0]
+        # sample the reparameterized latent distribution with a set of random variable 'epsilon'.
+        eps = tf.random_normal([batch_size, self.code_length], stddev=0.5)
+        sampled_latent_reps = variational_mean + tf.multiply(eps, variational_log_sigma)
+
+        return 0
