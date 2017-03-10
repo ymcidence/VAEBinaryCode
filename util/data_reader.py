0791d46fba64a9215308f05dde4c738c0c7bf70f
ymcidence <ym_zmxncbv@hotmail.com>
2017-03-10 11:34:23 +0000

    CVAE hehe

--- util/data_reader.py
+++ util/data_reader.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def reader_feat_only(i, mode=0):
-    total_batches = 145
-    training_batches = 130
+    total_batches = 1030
+    training_batches = 1015
     test_batches = total_batches - training_batches
     data_folder = 'E:\\WorkSpace\\Data\\sun397\\Batch2\\'
     if mode == 0:
@@ -16,3 +16,23 @@ def reader_feat_only(i, mode=0):
     batch['batch_feat'] = np.asarray(mat_file['batch_feat'], dtype=np.float32)
     batch['batch_label'] = np.asarray(mat_file['batch_label'], dtype=np.float32)
     return batch
+
+
+def reader_cifar10(i, mode=0):
+    total_batches = 600
+    training_batches = 590
+    test_batches = total_batches - training_batches
+    data_folder = 'E:\\WorkSpace\\Data\\CIFAR10\\Batch\\'
+    if mode == 0:
+        file_name = data_folder + 'batch_' + str(i % training_batches + 1) + '.mat'
+    else:
+        file_name = data_folder + 'batch_' + str(i % test_batches + 1 + training_batches) + '.mat'
+    mat_file = sio.loadmat(file_name)
+    batch = dict()
+    batch['batch_feat'] = np.asarray(mat_file['batch_feat'], dtype=np.float32)
+    batch['batch_label'] = np.asarray(mat_file['batch_label'], dtype=np.float32)
+    return batch
+
+
+def code_label_saver(codes, labels):
+    sio.savemat('E:\\WorkSpace\\WorkSpace\\ITQ\\hehe3.mat', {'codes': codes, 'labels': labels})
