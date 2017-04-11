28da8d9b7a778ba33c29586f5f42bb8e7f0e07a0
ymcidence <ym_zmxncbv@hotmail.com>
2017-04-11 17:04:52 +0100

    CVAE hehe

--- model/conditional_binaries.py
+++ model/conditional_binaries.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
-import cmath
 import numpy as np
 import gc
 from model import net_proto
 from time import gmtime, strftime
 from util import eval_tools
+from util import layers
 
 
 def loss_regu(par_list, weight=0.005):
@@ -31,7 +31,7 @@ class ConditionalVairationalBinaries(object):
 
         self.nets = self._build_graph()
         self.loss = self._build_loss()
-        self.opt = tf.train.AdamOptimizer(0.0001).minimize(self.loss)
+        self.opt = tf.train.AdamOptimizer(0.0001).minimize(self.loss, global_step=self.global_step)
         self.out_tensor = tf.to_float(tf.greater(self.nets[4], 0))
 
     def _build_graph(self):
@@ -80,10 +80,11 @@ class ConditionalVairationalBinaries(object):
         # learning objective 3: orthogonality
         loss_3 = tf.nn.l2_loss(
             tf.matmul(self.nets[4], self.nets[4], transpose_b=True) - tf.eye(self.batch_size, self.batch_size))
+        # loss_3 = -1 * tf.trace(tf.matmul(self.nets[4], self.nets[4], transpose_a=True)) / (self.code_length * 2) * 100
         tf.summary.scalar(net_proto.PHASE_TRAIN + '/losses/code_variance', loss_3)
 
-        # learning objective 4: Laplacian-graph-based loss
-        corr_matrix = net_proto.laplacian_graph(self.img_in)
+        # learning objective 4: Laplacian-graph-based loss````````
+        corr_matrix = net_proto.laplacian_graph(self.img_in, self.batch_size)
         norm_z = tf.nn.l2_normalize(self.nets[5], 1)
         product = tf.matmul(tf.matmul(norm_z, corr_matrix, transpose_a=True), norm_z)
         loss_4 = -tf.trace(product)
@@ -94,11 +95,13 @@ class ConditionalVairationalBinaries(object):
 
         # learning objective r: L2 regularization of the network
         loss_r = loss_regu(tf.trainable_variables())
-
-        #
-        loss_z = tf.nn.l2_loss(self.nets[-1] - self.latent_feature)
-
         tf.summary.scalar(net_proto.PHASE_TRAIN + '/losses/regu', loss_r)
+        #
+        # cls = layers.fc_layer('hehe', self.nets[-2], self.label_num)
+        # loss_z = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=self.data_label, logits=cls))
+
+        loss_z = tf.nn.l2_loss(self.nets[-1] - self.latent_feature)
+        tf.summary.scalar(net_proto.PHASE_TRAIN + '/losses/zz', loss_z)
 
         loss_out = 0.8 * loss_1 + loss_2 + loss_3 + loss_4 + loss_r + loss_z
         tf.summary.scalar(net_proto.PHASE_TRAIN + '/losses/total_loss', loss_out)
@@ -109,20 +112,25 @@ class ConditionalVairationalBinaries(object):
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
 
+        if self.restore_file is not None:
+            self._restore()
+
         max_epoch = 40
         train_interval, test_interval = dataset.iter_num()
 
         summary_op = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES, scope=net_proto.PHASE_TRAIN))
         summary_op_eval = tf.summary.merge(tf.get_collection(tf.GraphKeys.SUMMARIES, scope=net_proto.PHASE_EVAL))
 
-        writer = tf.summary.FileWriter(log_path + '/')
+        time_string = strftime("%a%d%b%Y-%H%M%S", gmtime())
+        writer = tf.summary.FileWriter(log_path + '/' + time_string + '/')
         saver = tf.train.Saver()
 
         for i in range(max_epoch):
             for j in range(train_interval):
-                batch_data = dataset.next_batch_train()
+                batch_data, batch_cluster = dataset.next_batch_train()
                 batch_code, summaries, this_loss, _ = self.sess.run([self.out_tensor, summary_op, self.loss, self.opt],
-                                                                    feed_dict={self.img_in: batch_data})
+                                                                    feed_dict={self.img_in: batch_data,
+                                                                               self.latent_feature: batch_cluster})
                 step = tf.train.global_step(self.sess, self.global_step)
                 writer.add_summary(summaries, global_step=step)
                 dataset.apply_code(batch_code)
@@ -145,7 +153,7 @@ class ConditionalVairationalBinaries(object):
             eval_to_write = self.sess.run(summary_op_eval,
                                           feed_dict={self.eval_map: np.asarray([mean_average_precision])})
             writer.add_summary(eval_to_write, global_step=step)
-            saver.save(self.sess, save_path + '\\hehemodel', global_step=step)
+            saver.save(self.sess, save_path + '\\ymmodel', global_step=step)
             print(str(mean_average_precision))
             dataset.reshuffle()
             del eval_to_write, mean_average_precision
@@ -153,3 +161,29 @@ class ConditionalVairationalBinaries(object):
 
     def _forward(self, batch_data):
         return self.sess.run(self.out_tensor, feed_dict={self.img_in: batch_data})
+
+    def _restore(self, restore_file=None):
+        saver = tf.train.Saver()
+        if restore_file is None:
+            restore_file = self.restore_file
+        return saver.restore(self.sess, restore_file)
+
+    def run_extraction(self, dataset, save_path):
+        train_interval, test_interval = dataset.iter_num()
+        init_op = tf.global_variables_initializer()
+        self.sess.run(init_op)
+        if self.restore_file is not None:
+            self._restore()
+
+        for i in range(train_interval):
+            print(str(i))
+            batch_feat, _ = dataset.next_batch_train()
+            batch_code = self._forward(batch_feat)
+            dataset.apply_code(batch_code)
+
+        for i in range(test_interval):
+            batch_feat = dataset.next_batch_test()
+            batch_code = self._forward(batch_feat)
+            dataset.apply_code(batch_code, 1)
+
+        dataset.save(save_path)
